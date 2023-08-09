cc1acdde647decd4a301223a572d4e1ce05de2d5
AkilaTS <108341855+AkilaTS@users.noreply.github.com>
2023-08-10 01:28:18 +0530

    Update ospf.py

--- ospf.py
+++ ospf.py
@@ -1,13 +1,3 @@
-# NAME: Akila Tharini Sivakumar
-# Roll Number: CS20B006
-# Course: CS3205 Jan. 2023 semester
-# Lab number: 5
-# Date of submission: 27.04.2023
-# I confirm that the source file is entirely written by me without
-# resorting to any dishonest means.
-# Website(s) that I used for basic socket programming code are:
-# URL(s): https://pythontic.com/modules/socket/udp-client-server-example
-
 import socket
 import os
 import sys
